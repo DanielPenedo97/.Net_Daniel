90e34c1218aed6924ee231f9c1f4549671a3e23e
DanielPenedo97 <dapsantos.cic@uesc.br>
2023-11-09 17:09:07 -0300

    Primeiro commit

---
+++ Semana1/PI001/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region Teste de Tipos de Dados
+
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    long tipoLong;
+
+    tipoInteiro = int.MaxValue;
+    tipoDouble = double.MaxValue;
+    tipoString = "100";
+    //tipoInteiro = int.Parse(tipoString);
+    tipoLong = long.MaxValue;
+
+    System.Console.WriteLine("O maximo inteiro eh: "+tipoInteiro);
+    System.Console.WriteLine("O maximo Long eh: "+tipoLong);
+    System.Console.WriteLine("O maximo Double eh: "+tipoDouble);
+    System.Console.WriteLine("String eh: "+tipoString);
+
+#endregion
+
+#region Operadores 
+
+#endregion
