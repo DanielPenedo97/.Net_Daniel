87ef4b1f6431112892e68f20b31a366b875365a2
DanielPenedo97 <dapsantos.cic@uesc.br>
2023-11-30 17:14:25 -0300

    commit 17:14

--- Semana4/Avaliacao_FInal/Program.cs
+++ Semana4/Avaliacao_FInal/Program.cs
@@ -5,31 +5,33 @@ using _Advogados;
 using _Cliente;
 using System.Security.Cryptography.X509Certificates;
 
-CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("pt-BR");
 
-#region Herança
-
-class Principal{
-    static void Main(){
+class Program{
+    public static void Main(){
+        CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("pt-BR");
         List<Advogados> advogados = new List<Advogados>(){
-            new Advogados("João Advogado", new DateTime(1, 1, 2001), "12345678901", "CNA123"),
-            new Advogados("Maria Advogada", new DateTime(03, 06, 2000), "23456789012", "CNA456"),
-            new Advogados("Pedro Advogado", new DateTime(2, 7, 1997), "34567890123", "CNA789")
+            new Advogados("João Advogado", new DateTime(1997, 2, 2), "12345678901", "CNA123"),
+            new Advogados("Maria Advogada", new DateTime(1997, 2, 2), "23456789012", "CNA456"),
+            new Advogados("Pedro Advogado", new DateTime(1997, 2, 2), "34567890123", "CNA789")
         };
 
         List<Cliente> clientes = new List<Cliente>(){
-            new Cliente("João Cliente", new DateTime(1, 1, 2001), "12345678901", "Solteiro", "Programador"),
-            new Cliente("Maria Cliente", new DateTime(03, 06, 2000), "23456789012", "Casado", "Programador"),
-            new Cliente("Pedro Cliente", new DateTime(2, 7, 1997), "34567890123", "Solteiro", "Programador")
+            new Cliente("João Cliente", new DateTime(1997, 2, 2), "12345678901", "Solteiro", "Programador"),
+            new Cliente("Maria Cliente", new DateTime(1997, 2, 2), "23456789012", "Casado", "Programador"),
+            new Cliente("Pedro Cliente", new DateTime(1997, 2, 2), "34567890123", "Solteiro", "Programador"),
+            new Cliente("João Cliente", new DateTime(1997, 2, 2), "12345678901", "Solteiro", "Programador"),
         };
-        
-    Console.WriteLine("Advogados: ");
-        foreach(Advogados advogado in advogados){
-            Console.WriteLine($"Nome: {advogado.Nome}");
-            Console.WriteLine($"CPF: {advogado.Cpf}");
-        }
-    
-    }
 
+
+        Console.WriteLine("Advogados: ");
+            foreach(Advogados advogado in advogados){
+                Console.WriteLine($"Nome: {advogado.Nome}");
+                Console.WriteLine($"CPF: {advogado.Cpf}");
+            }
+        Console.WriteLine("Clientes: ");
+            foreach(Cliente cliente in clientes){
+                Console.WriteLine($"Nome: {cliente.Nome}");
+                Console.WriteLine($"CPF: {cliente.Cpf}");
+            }
+    }
 }
-#endregion
