94f2f69a0e9ecfe0de6ff4fc6b4f78346fec0645
DanielPenedo97 <dapsantos.cic@uesc.br>
2023-11-30 15:34:44 -0300

    15.35

---
+++ Semana4/Avaliacao_FInal/Program.cs
@@ -0,0 +1,98 @@
+﻿#region 
+using System;
+
+class Advogados{
+    public string nome;
+    public string cna;
+    public string especialidade;
+
+    public string dataDeNascimento;
+
+    public string DataDeNascimento{
+        get{
+            return dataDeNascimento;
+        }
+        set{
+            dataDeNascimento = value;
+        }
+    }
+    
+    public string Nome{
+        get{
+            return nome;
+        }
+        set{
+            nome = value;
+        }
+    }
+    public string Cna{
+        get{
+            return cna;
+        }
+        set{
+            cna = value;
+        }
+    }
+    public string Especialidade{
+        get{
+            return especialidade;
+        }
+        set{
+            especialidade = value;
+        }
+    }
+    public Advogados(string nome, string cna, string especialidade, string dataDeNascimento){
+        this.nome = nome;
+        this.cna = cna;
+        this.especialidade = especialidade;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+    public void imprime(){
+        Console.WriteLine("Nome: {0}", this.nome);
+        Console.WriteLine("cna: {0}", this.cna);
+        Console.WriteLine("Especialidade: {0}", this.especialidade);
+        Console.WriteLine("Data de Nascimento: {0}", this.dataDeNascimento);
+    }
+}
+
+class Cliente{
+    public string nome;
+    public string cpf;
+    public string endereco;
+    public string Nome{
+        get{
+            return nome;
+        }
+        set{
+            nome = value;
+        }
+    }
+    public string Cpf{
+        get{
+            return cpf;
+        }
+        set{
+            cpf = value;
+        }
+    }
+    public string Endereco{
+        get{
+            return endereco;
+        }
+        set{
+            endereco = value;
+        }
+    }
+    public Cliente(string nome, string cpf, string endereco){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+    public void imprime(){
+        Console.WriteLine("Nome: {0}", this.nome);
+        Console.WriteLine("cpf: {0}", this.cpf);
+        Console.WriteLine("Endereço: {0}", this.endereco);
+    }
+}
+
+#endregion
--- Semana4/Exercicio4/Program.cs
+++ Semana4/Exercicio4/Program.cs
@@ -1,11 +1,18 @@
 ﻿#region 
 
 using System;
+using System.Net.WebSockets;
 
-    class Veiculo{
+class Veiculo{
         public string modelo;
         public string cor;
         public int ano;
+        public string IdadeVeiculo{
+            get{
+                int idade = DateTime.Now.Year - ano;
+                return idade.ToString();
+            }
+        }
         public Veiculo(string marca, string modelo, string cor, int ano, double preco){
             this.modelo = modelo;
             this.cor = cor;
@@ -15,6 +22,7 @@ using System;
             Console.WriteLine("Modelo: {0}", this.modelo);
             Console.WriteLine("Cor: {0}", this.cor);
             Console.WriteLine("Ano: {0}", this.ano);
+            Console.WriteLine("Idade do veículo: {0}", this.IdadeVeiculo);
         }
     }
 
@@ -27,6 +35,16 @@ using System;
             base.imprime();
             Console.WriteLine("Preço: {0}", this.preco);
         }
+        static void Main(){
+            Carro c = new Carro("Fiat", "Uno", "Vermelho", 2010, 15000);
+            c.imprime();
+        }
     }
+
+#endregion
+
+#region 
+
+
 
 #endregion
